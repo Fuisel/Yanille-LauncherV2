5c2ae2d476a0685cb0a0221d54b8be056b406420
Mark7625 <72366279+Mark7625@users.noreply.github.com>
2023-01-09 20:38:09 +0000

    Launcher

---
+++ build-linux-aarch64.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+set -e
+
+JDK_VER="11.0.16.1"
+JDK_BUILD="1"
+JDK_HASH="b6607f28fa2906d612d517f0babe4f0f895aa1c3f901edcddb493e33c1e27364"
+PACKR_VERSION="runelite-1.7"
+PACKR_HASH="f61c7faeaa364b6fa91eb606ce10bd0e80f9adbce630d2bae719aef78d45da61"
+APPIMAGE_VERSION="13"
+
+umask 022
+
+if ! [ -f OpenJDK11U-jre_aarch64_linux_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz ] ; then
+    curl -Lo OpenJDK11U-jre_aarch64_linux_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz \
+        https://github.com/adoptium/temurin11-binaries/releases/download/jdk-${JDK_VER}%2B${JDK_BUILD}/OpenJDK11U-jre_aarch64_linux_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz
+fi
+
+echo "${JDK_HASH} OpenJDK11U-jre_aarch64_linux_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz" | sha256sum -c
+
+# packr requires a "jdk" and pulls the jre from it - so we have to place it inside
+# the jdk folder at jre/
+if ! [ -d linux-aarch64-jdk ] ; then
+    tar zxf OpenJDK11U-jre_aarch64_linux_hotspot_${JDK_VER}_${JDK_BUILD}.tar.gz
+    mkdir linux-aarch64-jdk
+    mv jdk-$JDK_VER+$JDK_BUILD-jre linux-aarch64-jdk/jre
+fi
+
+if ! [ -f packr_${PACKR_VERSION}.jar ] ; then
+    curl -Lo packr_${PACKR_VERSION}.jar \
+        https://github.com/runelite/packr/releases/download/${PACKR_VERSION}/packr.jar
+fi
+
+echo "${PACKR_HASH}  packr_${PACKR_VERSION}.jar" | sha256sum -c
+
+# Note: Host umask may have checked out this directory with g/o permissions blank
+chmod -R u=rwX,go=rX appimage
+# ...ditto for the build process
+chmod 644 target/Vanguard.jar
+
+rm -rf native-linux-aarch64
+
+java -jar packr_${PACKR_VERSION}.jar \
+    packr/linux-aarch64-config.json
+
+pushd native-linux-aarch64/Vanguard.AppDir
+mkdir -p jre/lib/amd64/server/
+ln -s ../../server/libjvm.so jre/lib/amd64/server/ # packr looks for libjvm at this hardcoded path
+
+# Symlink AppRun -> RuneLite
+ln -s Vanguard AppRun
+
+# Ensure RuneLite is executable to all users
+chmod 755 Vanguard
+popd
+
+if ! [ -f appimagetool-x86_64.AppImage ] ; then
+    curl -Lo appimagetool-x86_64.AppImage \
+        https://github.com/AppImage/AppImageKit/releases/download/$APPIMAGE_VERSION/appimagetool-x86_64.AppImage
+    chmod +x appimagetool-x86_64.AppImage
+fi
+
+echo "df3baf5ca5facbecfc2f3fa6713c29ab9cefa8fd8c1eac5d283b79cab33e4acb  appimagetool-x86_64.AppImage" | sha256sum -c
+
+if ! [ -f runtime-aarch64 ] ; then
+    curl -Lo runtime-aarch64 \
+	    https://github.com/AppImage/AppImageKit/releases/download/$APPIMAGE_VERSION/runtime-aarch64
+fi
+
+echo "d2624ce8cc2c64ef76ba986166ad67f07110cdbf85112ace4f91611bc634c96a  runtime-aarch64" | sha256sum -c
+
+ARCH=arm_aarch64 ./appimagetool-x86_64.AppImage \
+	--runtime-file runtime-aarch64  \
+	native-linux-aarch64/Vanguard.AppDir/ \
+	native-linux-aarch64/Vanguard-aarch64.AppImage
---
+++ innosetup/app.iss
@@ -0,0 +1,57 @@
+[Setup]
+AppName=Vanguard Launcher
+AppPublisher=Vanguard
+UninstallDisplayName=Vanguard
+AppVersion=${project.version}
+AppSupportURL=https://www.vanguard317.com/
+DefaultDirName={localappdata}\Vanguard
+
+; ~30 mb for the repo the launcher downloads
+ExtraDiskSpaceRequired=30000000
+ArchitecturesAllowed=x64
+PrivilegesRequired=lowest
+
+WizardSmallImageFile=${basedir}/app_small.bmp
+WizardImageFile=${basedir}/left.bmp
+SetupIconFile=${basedir}/app.ico
+UninstallDisplayIcon={app}\Vanguard.exe
+
+Compression=lzma2
+SolidCompression=yes
+
+OutputDir=${basedir}
+OutputBaseFilename=VanguardSetup64
+
+[Tasks]
+Name: DesktopIcon; Description: "Create a &desktop icon";
+
+[Files]
+Source: "${basedir}\app.ico"; DestDir: "{app}"
+Source: "${basedir}\left.bmp"; DestDir: "{app}"
+Source: "${basedir}\app_small.bmp"; DestDir: "{app}"
+Source: "${basedir}\native-win64\Vanguard.exe"; DestDir: "{app}"
+Source: "${basedir}\native-win64\Vanguard.jar"; DestDir: "{app}"
+Source: "${basedir}\native\build64\Release\launcher_amd64.dll"; DestDir: "{app}"
+Source: "${basedir}\native-win64\config.json"; DestDir: "{app}"
+Source: "${basedir}\native-win64\jre\*"; DestDir: "{app}\jre"; Flags: recursesubdirs
+
+[Icons]
+; start menu
+Name: "{userprograms}\Vanguard"; Filename: "{app}\Vanguard.exe"
+Name: "{userdesktop}\Vanguard"; Filename: "{app}\Vanguard.exe"; Tasks: DesktopIcon
+
+[Run]
+Filename: "{app}\Vanguard.exe"; Parameters: "--postinstall"; Flags: nowait
+Filename: "{app}\Vanguard.exe"; Description: "&Open Vanguard"; Flags: postinstall skipifsilent nowait
+
+[InstallDelete]
+; Delete the old jvm so it doesn't try to load old stuff with the new vm and crash
+Type: filesandordirs; Name: "{app}\jre"
+
+[UninstallDelete]
+Type: filesandordirs; Name: "{%USERPROFILE}\.vanguard\repository2"
+; includes install_id, settings, etc
+Type: filesandordirs; Name: "{app}"
+
+[Code]
+#include "upgrade.pas"
